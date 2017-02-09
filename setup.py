db493de20a7048159c3fabac13245aff8719d981
Abel Heinsbroek <mail@abelheinsbroek.nl>
2017-02-09 12:25:31 +0100

    Python3 compatibility

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 
 def fetch_viphreeqc():
-    print "Fetching latest VIPhreeqc build from ci..."
+    print("Fetching latest VIPhreeqc build from ci...")
     from_zip = False
     if sys.platform == "darwin":
         dll_name = "viphreeqc.dylib"
@@ -18,19 +18,19 @@ def fetch_viphreeqc():
 
     
     dll_path = os.path.dirname(os.path.abspath(__file__)) + os.sep + "phreeqpython" + os.sep + "lib" + os.sep +dll_name
-    print "Downloading to:", dll_path
+    print("Downloading to:", dll_path)
 
     urllib.urlretrieve("http://ci.abelheinsbroek.nl/"+dll_name, dll_path)
 
     if dll_name == "viphreeqc.zip":
         with zipfile.ZipFile(dll_path,"r") as archive:
             extract_path = os.path.dirname(os.path.abspath(__file__)) + "\phreeqpython\lib"
-            print "Unpacking library to:", extract_path
+            print("Unpacking library to:", extract_path)
             archive.extractall(extract_path)
         os.remove(dll_path)
 
 
-    print "Done!"
+    print("Done!")
     
 
 class PostDevelopCommand(develop):
