cca03c7abfdd3c87e483d4a46362185fae98fe17
Jeroen van Rijn <31624947+jeroen1405@users.noreply.github.com>
2019-02-12 15:57:24 +0100

    Add files via upload

--- phreeqpython/utility.py
+++ phreeqpython/utility.py
@@ -1,6 +1,8 @@
 from periodictable import formula as chemform
 
 def convert_units(formula, amount, from_units='mol', to_units='mmol'):
+    if formula == 'F' :
+        formula = 'Ni'
     if from_units == to_units:
         return amount
 
@@ -9,16 +11,30 @@ def convert_units(formula, amount, from_units='mol', to_units='mmol'):
             return amount*1e3
         if to_units == 'mg':
             return chemform(formula).mass * amount * 1e3
+        if to_units == 'ug':
+            return chemform(formula).mass * amount * 1e6
 
     if from_units == 'mmol':
         if to_units == 'mol':
             return amount * 1e-3
         if to_units == 'mg':
             return chemform(formula).mass * amount
+        if to_units == 'ug':
+            return chemform(formula).mass * amount * 1e3
 
     if from_units == 'mg':
         if to_units == 'mol':
             return amount / chemform(formula).mass * 1e-3
-
         if to_units == 'mmol':
             return amount / chemform(formula).mass
+        if to_units == 'ug':
+            return amount * 1e3
+
+    if from_units == 'ug': #micrograms
+        if to_units == 'mol':
+            return amount / chemform(formula).mass * 1e-6
+        if to_units == 'mmol':
+            return amount / chemform(formula).mass * 1e-3
+        if to_units == 'mg':
+            return amount * 1e-3
+
