6c8a1095dffd22d0849c60ee612100ad19ae299f
Nils Zickermann <nils.zickermann@vitens.com>
2026-04-20 15:43:43 +0200

    bump version number to 1.5.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import zipfile
 import urllib
 
 setup(name='phreeqpython',
-      version='1.5.7',
+      version='1.5.8',
       description='Vitens viphreeqc wrapper and utilities',
       url='https://github.com/Vitens/phreeqpython',
       author='Abel Heinsbroek',
